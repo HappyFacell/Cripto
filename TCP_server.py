4e4400719652c1f968dcce01fe88217d65b16a3a
Armando Ferreira <is722820@iteso.mx>
2022-04-22 14:56:11 -0500

    Send file to TCP server

--- TCP_server.py
+++ TCP_server.py
@@ -1,19 +1,26 @@
-import base64
 import socket
-import Pseudo_random
 
 HOST = "127.0.0.1"
 PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(Pseudo_random.random()) 
-            # conn.sendall(base64.b64decode(Pseudo_random.random())) #Dato original
+
+def main() -> None:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            file_data = b"" # Datos del archivo
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                file_data += data # Guardamos los datos recibidos para escribirlos en el archivo de servidor
+
+    with open("./server/file_to_recive.txt", "wb") as file: # Creamos el archivo y escribimos en el
+        file.write(file_data)
+
+
+if __name__ == "__main__":
+    main()
